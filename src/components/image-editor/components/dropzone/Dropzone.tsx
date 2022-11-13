5691ce4685af759b0218184d299061939444568b
Maciej Grzybek <maciekgrzybek1@gmail.com>
2022-11-13 14:31:10 +0100

    feat: add more stuff to dropzone

--- src/components/image-editor/components/dropzone/Dropzone.tsx
+++ src/components/image-editor/components/dropzone/Dropzone.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
 
@@ -7,19 +7,36 @@ type Props = {
 };
 
 export const Dropzone = ({ handleDrop }: Props) => {
+  const [error, setError] = useState<Error | null>(null);
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
+      console.log({ acceptedFiles });
       handleDrop(acceptedFiles[0]);
+      clearError();
     },
     [handleDrop]
   );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+  const handleError = (error: Error) => setError(error);
+  const clearError = () => setError(null);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    onError: handleError,
+
+    multiple: false,
+    accept: {
+      'image/jpeg': ['.jpeg', '.jpg'],
+      'image/png': ['.png'],
+      'image/webp': ['.webp'],
+    },
+  });
+  console.log({ error });
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
 
-      <div className="border-dashed border-2 rounded-2xl flex justify-center items-center bg-blue-50 flex-col w-full py-20">
+      <div className="border-dashed border-2 rounded-2xl flex justify-center items-center bg-blue-50 flex-col w-full h-72">
         <AiOutlineCloudUpload className="w-10 h-10 mb-4" />
 
         <span className="flex flex-col text-center text-sm">
@@ -27,7 +44,6 @@ export const Dropzone = ({ handleDrop }: Props) => {
             'Dropping file...'
           ) : (
             <>
-              {' '}
               <span className="text-blue-500 mb-1">
                 Drag and drop your profile picture
               </span>
@@ -38,6 +54,11 @@ export const Dropzone = ({ handleDrop }: Props) => {
             </>
           )}
         </span>
+        {error && (
+          <span className="text-red-400 mt-6">
+            This filetype is not accepted. Use jpeg, png or webp.
+          </span>
+        )}
       </div>
     </div>
   );
