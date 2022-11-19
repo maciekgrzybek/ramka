a8dd20c6565e1b5e5389fd7ba8020b3d1ce7eeab
Maciej Grzybek <maciekgrzybek1@gmail.com>
2022-11-19 19:25:06 +0100

    chore: heck knows

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePluginFonts } from 'vite-plugin-fonts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
